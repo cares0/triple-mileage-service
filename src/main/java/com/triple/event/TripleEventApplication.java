34cbc64b8c5c62a259e3b0325f9942637efe65b9
cares0 <cares00000@gmail.com>
2022-06-28 12:40:02 +0900

    init project

---
+++ src/main/java/com/triple/event/TripleEventApplication.java
@@ -0,0 +1,13 @@
+package com.triple.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleEventApplication.class, args);
+    }
+
+}
