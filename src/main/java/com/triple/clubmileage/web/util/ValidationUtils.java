cbbde31852d1789579e40c9881bfa182dde057a6
cares0 <cares00000@gmail.com>
2022-06-30 20:44:50 +0900

    refeactor: 검증 예외 처리 코드 리팩토링
    람다, 스트림 활용

--- src/main/java/com/triple/clubmileage/web/util/ValidationUtils.java
+++ src/main/java/com/triple/clubmileage/web/util/ValidationUtils.java
@@ -4,24 +4,18 @@ import com.triple.clubmileage.web.exception.ValidationFailException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class ValidationUtils {
 
     public void validate(BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            Map<String, String> map = new HashMap<>();
-            for (ObjectError objectError : bindingResult.getAllErrors()) {
-                String defaultMessage = objectError.getDefaultMessage();
-                FieldError fieldError = (FieldError) objectError;
-                String fieldName = fieldError.getField();
-                map.put(fieldName, defaultMessage);
-            }
-            throw new ValidationFailException(map.toString());
+            Map<String, String> errorMap = bindingResult.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            throw new ValidationFailException(errorMap.toString());
         }
     }
 }
